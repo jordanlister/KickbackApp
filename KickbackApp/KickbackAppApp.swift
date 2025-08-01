c6e276c04c51debc540fa4a597437278f00da683
Jordan Lister <jordanlister@users.noreply.github.com>
2025-08-01 00:35:31 -0700

    Initial Commit

---
+++ KickbackApp/KickbackAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KickbackAppApp.swift
+//  KickbackApp
+//
+//  Created by Jordan Lister on 8/1/25.
+//
+
+import SwiftUI
+
+@main
+struct KickbackAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
